e4247e8afe0d83f52f65c71ced68af229c5a5836
dennis iluma <dennismac@denniss-MacBook-Pro.local>
2022-05-24 23:31:00 +0100

    api done for now

---
+++ studentportalapi/src/main/java/com/studentportal/studentportalapi/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.studentportal.studentportalapi.controller;
+
+import com.studentportal.studentportalapi.model.Student;
+import com.studentportal.studentportalapi.service.StudentService;
+import com.studentportal.studentportalapi.service.StudentServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/add")
+    public String addStudent( @RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> findALlStudents(){
+        return studentService.findAllStudent();
+    }
+}
